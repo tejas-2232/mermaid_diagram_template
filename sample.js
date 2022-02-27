ebbfdd91f30f31abe3695934a35ece6075fbf2ea
Tejas Bachhav <45887773+tejas-2232@users.noreply.github.com>
2022-02-27 21:43:53 +0530

    Update and rename ganttchart to sample.js

---
+++ sample.js
@@ -0,0 +1,8 @@
+journey
+    title Apache role development
+    section WorkFlow
+        Create repo: 2: Ron
+        Clone repo: 3: Sam
+        Develop role: 4: Tom, Curan
+        Lint Test: 5: tennyson, Anaisa
+        Create ASTRa role: 6: Peter Parker
